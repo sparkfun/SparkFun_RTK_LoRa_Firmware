c176b47a0df9977f2dbff4c8fd70d70e16275ecc
PaulZC <pc235603@gmail.com>
2026-07-28 08:41:54 +0100

    Update dma.c

--- RTCM_TRX_FSS_RTK/Core/Src/dma.c
+++ RTCM_TRX_FSS_RTK/Core/Src/dma.c
@@ -55,12 +55,12 @@ void MX_DMA_Init(void)
   HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 5, 2); // uart2 tx
   HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
 
-  /* DMA1_Channel7_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel7_IRQn, 5, 0); // uart1 rx
-  HAL_NVIC_EnableIRQ(DMA1_Channel7_IRQn);
   /* DMA1_Channel6_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel6_IRQn, 5, 2); // uart1 tx
   HAL_NVIC_EnableIRQ(DMA1_Channel6_IRQn);
+  /* DMA1_Channel7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel7_IRQn, 5, 0); // uart1 rx
+  HAL_NVIC_EnableIRQ(DMA1_Channel7_IRQn);
 
 }
 

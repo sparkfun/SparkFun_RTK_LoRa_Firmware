9fc1caf16aa798d4ae48c044c3f38418c05006dc
PaulZC <pc235603@gmail.com>
2026-07-31 12:30:53 +0100

    v3.0.3 - restart uart on Frame Error

--- RTCM_TRX_FSS_RTK/SubGHz_Phy/App/drv_uart.c
+++ RTCM_TRX_FSS_RTK/SubGHz_Phy/App/drv_uart.c
@@ -415,13 +415,24 @@ void UART_ErrorCallback(UART_HandleTypeDef *huart)
     //     APP_TPRINTF("uart1 to\r\n");
     //     start_uart_rx_irq(huart);
     // }
-	APP_TPRINTF("Uart[%d] ErrorCode:0x%x\r\n",(huart->Instance ==USART2), huart->ErrorCode);
+	APP_TPRINTF("Uart[%d] ErrorCode:0x%x%s\r\n",(huart->Instance == USART1) ? 1 : 2, huart->ErrorCode,
+        huart->ErrorCode == HAL_UART_ERROR_NONE ? " (NONE)" :
+        huart->ErrorCode == HAL_UART_ERROR_PE ? " (PE)" :
+        huart->ErrorCode == HAL_UART_ERROR_NE ? " (NE)" :
+        huart->ErrorCode == HAL_UART_ERROR_FE ? " (FE)" :
+        huart->ErrorCode == HAL_UART_ERROR_ORE ? " (ORE)" :
+        huart->ErrorCode == HAL_UART_ERROR_DMA ? " (DMA)" :
+        huart->ErrorCode == HAL_UART_ERROR_RTO ? " (RTO)" : "");
     APP_TPRINTF("--- uart error callback ---Free heap memory: %d bytes------\r\n", xPortGetFreeHeapSize());
-	if(huart->ErrorCode == HAL_UART_ERROR_DMA)
+	if(huart->ErrorCode == HAL_UART_ERROR_DMA) // DMA transfer error
 	{
 		start_uart_rx_irq(huart);// DMA error,data will drop
 	}
-    if (huart->ErrorCode == HAL_UART_ERROR_ORE)
+	if(huart->ErrorCode == HAL_UART_ERROR_FE) // Frame error
+	{
+		start_uart_rx_irq(huart);// DMA error,data will drop
+	}
+    if (huart->ErrorCode == HAL_UART_ERROR_ORE) // Overrun error
     {
         DRV_TRANS_DATA_TYPE data;
         BaseType_t xHigherPriorityTaskWoken;
